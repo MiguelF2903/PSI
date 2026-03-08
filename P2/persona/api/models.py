ab5c7233dcbb4a71f2b5880aa23179fcb612aac4
diego <diego.ruz@estudiante.uam.es>
2026-03-08 16:47:45 +0100

    Añadido backend Django con PostgreSQL

---
+++ P2/persona/api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Persona(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
